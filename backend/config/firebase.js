ae675e5dfb938a04255d34ae5103d5d3496119d5
Mahmaduvesh <uveshkhalifa7920@gmail.com>
2026-07-27 15:06:22 +0530

    update backend firbase config

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -3,7 +3,7 @@ const serviceAccount = require("./firebase-adminsdk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://gym-management-50f61-default-rtdb.firebaseio.com/",
+  databaseURL: "https://gym-management-50f61-default-rtdb.firebaseio.com",
 });
 
 module.exports = admin;
